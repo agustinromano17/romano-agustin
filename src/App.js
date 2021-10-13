63a0e041168cdd3c1856f95d6f4555a020760b16
Agustin Romano <romanoagustin17@hotmail.com>
2021-10-13 20:33:11 -0300

    Agregue react bootstrap, reactrap, cree la carpeta components en src, con el archivo NavBar.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from  "./components/NavBar"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,9 +13,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Estamos probando
         </a>
       </header>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+// IMPORTS - Requiero las librerias, componentes o lo que necesite arriba de todo
+
+// COMPONENT - Desarrollo lo que necesito que haga mi componente
+
+// EXPORT - Exportar el componente para poder utilizarlo en otras partes del codigo
+import { Button } from "react-bootstrap"
+
+const NavBar = () => {
+
+    //Logic Develop
+    return(
+    <>
+        <h2>Soy un texto en Navbar</h2>
+        <Button variant="primary">Boton 1</Button>{""}
+    </>
+    )
+}
+export default NavBar
